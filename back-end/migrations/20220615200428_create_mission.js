7ac305a6f761f3f95da6e57510e8290434819212
Cybyl Jade <cybyljade@Cybyls-MacBook-Pro.local>
2022-06-15 15:53:44 -0600

    mission and users table created

---
+++ back-end/migrations/20220615200428_create_mission.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('mission', table => {
+    table.increments();
+    table.string('statement', 500);
+    table.float('location_long');
+    table.float('location_lat');
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('mission');
+};
